f19d5d84da0d281776adebe1c468d60436877875
GANESH <ganeshnarvekar09@gmail.com>
2024-04-05 14:08:14 +0530

    Added a new API Lambok and updated all domain classes According to Lombok

--- src/main/java/home/pratice/domain/Book.java
+++ src/main/java/home/pratice/domain/Book.java
@@ -1,15 +1,23 @@
 package home.pratice.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Data
 @Entity
 @NamedQueries({
         @NamedQuery(name = "getBooksByAuthorName",
-                    query = "FROM Book book " +
-                            "WHERE book.autherName = :autherName"),
-        @NamedQuery(name="getBooksByBookName",
-                    query = "FROM Book book " +
-                "WHERE book.bookName = :bookName")
+                query = "FROM Book book " +
+                        "WHERE book.autherName = :autherName"),
+        @NamedQuery(name = "getBooksByBookName",
+                query = "FROM Book book " +
+                        "WHERE book.bookName = :bookName")
 
 })
 public class Book {
@@ -28,65 +36,4 @@ public class Book {
     private String isbnNumber;
     @Column
     private Integer price;
-
-
-    public String getAutherName() {
-        return autherName;
-    }
-
-    public void setAutherName(String autherName) {
-        this.autherName = autherName;
-    }
-
-    public String getIsbnNumber() {
-        return isbnNumber;
-    }
-
-    public void setIsbnNumber(String isbnNumber) {
-        this.isbnNumber = isbnNumber;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public int getBookNumber() {
-        return bookNumber;
-    }
-
-    public Long getBookId() {
-        return bookId;
-    }
-
-    public void setBookId(Long bookId) {
-        this.bookId = bookId;
-    }
-
-    public String getBookName() {
-        return bookName;
-    }
-
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
-    }
-
-    public void setBookNumber(int bookNumber) {
-        this.bookNumber = bookNumber;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "bookId=" + bookId +
-                ", bookNumber=" + bookNumber +
-                ", bookName='" + bookName + '\'' +
-                ", autherName='" + autherName + '\'' +
-                ", isbnNumber=" + isbnNumber +
-                ", price=" + price +
-                '}';
-    }
 }
